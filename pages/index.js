d42a17237567221cc4d873d2f127aa0a8a8ec803
ekalii <andoune0719@gmail.com>
2020-06-30 14:03:06 +0900

    build postcard

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import PostForm from '../components/postform';
+import PostCard from '../components/postcard';
+
+const mock = {
+	isLoggedIn: true,
+	imagePaths: [],
+	mainPosts: [{
+		User: {
+			id: 1,
+			nickname: 'Ekalii',
+		},
+		content: 'First Card',
+		img: 'https://avatars0.githubusercontent.com/u/16658067?s=460&u=09f9f385ffec1453896c9b079cc4fd503ee4cded&v=4'
+	}],
+};
+
 export default function Home() {
   return (
     <>
-      <p>Helloworld!</p>
+      <div>
+      	{mock.isLoggedIn && <PostForm imagePaths={mock.imagePaths}/>}
+      	{
+      		mock.mainPosts.map(v => {
+      			return <PostCard key={v} post={v}/>
+      		})
+      	}
+      </div>
     </>
   );
 }
