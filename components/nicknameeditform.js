508f3aa9826b950da4ff9c81a50de03243df24fd
ekalii <andoune0719@gmail.com>
2020-06-30 14:26:19 +0900

    make nickname change form

---
+++ components/nicknameeditform.js
@@ -0,0 +1,10 @@
+import { Form, Input, Button } from 'antd';
+
+export default function NicknameEditForm() {
+	return(
+		<Form style={{ marginTop: '20px' }}>
+			<Input style={{ marginBottom: '10px' }} addonBefore="Nickname" />
+			<Button type="primary">Change</Button>
+		</Form>
+	);
+}
