dc402178b2cd47d57b438b6be8598794808cbee6
ekalii <andoune0719@gmail.com>
2020-07-01 15:22:36 +0900

    delete mockupdata

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,20 @@
+import { useSelector } from 'react-redux';
+
 import PostForm from '../components/postform';
 import PostCard from '../components/postcard';
 
-const mock = {
-	isLoggedIn: true,
-	imagePaths: [],
-	mainPosts: [{
-		User: {
-			id: 1,
-			nickname: 'Ekalii',
-		},
-		content: 'First Card',
-		img: 'https://avatars0.githubusercontent.com/u/16658067?s=460&u=09f9f385ffec1453896c9b079cc4fd503ee4cded&v=4'
-	}],
-};
-
 export default function Home() {
+  const { isLoggedIn } = useSelector(state => state.user);
+  const { mainPosts } = useSelector(state => state.post);
+
   return (
-    <>
-      <div>
-      	{mock.isLoggedIn && <PostForm imagePaths={mock.imagePaths}/>}
-      	{
-      		mock.mainPosts.map(v => {
-      			return <PostCard key={v} post={v}/>
-      		})
-      	}
-      </div>
-    </>
+    <div>
+    	{isLoggedIn && <PostForm />}
+    	{
+    		mainPosts.map(v => {
+    			return <PostCard key={v} post={v}/>
+    		})
+    	}
+    </div>
   );
 }
