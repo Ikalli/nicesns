5ac0ce7d9c570dcf8855ad5db53ee81014b5e239
ekalii <andoune0719@gmail.com>
2020-07-01 15:21:45 +0900

    apply redux and react-redux in project

---
+++ reducers/post.js
@@ -0,0 +1,38 @@
+export const initialState = {
+	imagePaths: [],
+	mainPosts: [{
+		User: {
+			id: 1,
+			nickname: 'MockNickName',
+		},
+		content: 'First Card',
+		img: 'https://avatars0.githubusercontent.com/u/16658067?s=460&u=09f9f385ffec1453896c9b079cc4fd503ee4cded&v=4'
+	}],
+};
+
+const ADD_POST = 'ADD_POST';
+const ADD_DUMMY = 'ADD_DUMMY';
+
+export const addPost = {
+	type: ADD_POST,
+};
+
+export const addDummy = {
+	type: ADD_DUMMY,
+};
+
+export default (state = initialState, action) => {
+	const { type } = action;
+	switch (type) {
+		case ADD_POST:
+			return {
+				...state,
+			};
+		case ADD_DUMMY:
+			return {
+				...state,
+			};
+		default:
+			return state;
+	}
+}
---
+++ reducers/user.js
@@ -0,0 +1,77 @@
+const mockUser = {
+	nickname: 'MockUserRedux',
+	Post: [],
+	Followings: [],
+	Followers: [],
+}
+
+export const initialState = {
+	isLoggedIn: false,
+	user: null,
+	signUpData: {},
+	loginData: {},
+};
+
+const SIGN_UP = 'SIGN_UP';
+const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
+const LOG_IN = 'LOG_IN';
+const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+const LOG_OUT = 'LOG_OUT';
+
+export const signUpAction = data => {
+	return {
+		type: SIGN_UP,
+		payload: {
+			signUpData: data
+		}
+	}
+};
+
+export const loginAction = data => {
+	return{
+		type: LOG_IN,
+		payload: {
+			loginData: data
+		}
+	}
+};
+
+export const logoutAction = () => {
+	return{
+		type: LOG_OUT
+	}
+};
+
+export default (state = initialState, action) => {
+	const { type, payload } = action;
+
+	switch(type){
+		case SIGN_UP:
+			return {
+				...state,
+				signUpData: payload.signUpData
+			};
+		case SIGN_UP_SUCCESS:
+			return state;
+		case LOG_IN:
+			return {
+				...state,
+				isLoggedIn: true,
+				user: mockUser,
+				loginData: payload.loginData
+			};
+		case LOG_IN_SUCCESS:
+			return state;
+		case LOG_IN_FAILURE:
+			return state;
+		case LOG_OUT:
+			return {
+				...state,
+				isLoggedIn: false,
+				user: {}
+			}
+		default:
+			return state;
+	}
+};
