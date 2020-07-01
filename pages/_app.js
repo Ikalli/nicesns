843ed948de98f4c06c97b29980ecdf2f5eee286a
ekalii <andoune0719@gmail.com>
2020-07-01 15:23:01 +0900

    error: withRedux and store error

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import Layout from '../components/layout';
+import withRedux from 'next-redux-wrapper';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducers from '../reducers';
+
 
 function NiceSns({ Component }) {
 	return(
-		<>
+		<Provider store={store}>
 			<Head>
 				<title>NiceSNS</title>
 				<link rel="stylesheet"
@@ -14,12 +19,23 @@ function NiceSns({ Component }) {
       <Layout>
       	<Component />
     	</Layout>
-  	</>
+  	</Provider>
 	);
 };
 
 NiceSns.PropTypes = {
-	Component: PropTypes.elementType
+	Component: PropTypes.elementType,
+	store: PropTypes.object,
 };
 
-export default NiceSns;
+const configureStore = (initialState, options) => {
+	const middlewares = [];
+	const enhancer = compose(
+		applyMiddleware(...middlewares)
+	);
+
+	const store = createStore(reducers, initialState, enhancer);
+	return store;
+}
+
+export default withRedux(configureStore)(NiceSns);
