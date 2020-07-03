ed98d244ebe75d793b2db6203dc0c9acb0f52bce
ekalii <andoune0719@gmail.com>
2020-07-03 15:23:45 +0900

    !!!error: need to mount sagaMiddleware to store

--- pages/_store.js
+++ pages/_store.js
@@ -1,9 +1,16 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { createWrapper } from 'next-redux-wrapper';
 import reducer from '../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../saga'
 
-const initStore = () => {
-	return createStore(reducer);
+const initStore = (initialState, options) => {
+	const sagaMiddleware = createSagaMiddleware();
+	const middlewares = [sagaMiddleware];
+	const enhancer = compose(applyMiddleware(...middlewares));
+	sagaMiddleware.run(rootSaga);
+
+	return createStore(reducer, initialState, enhancer);
 }
 
 export const wrapper = createWrapper(initStore);
