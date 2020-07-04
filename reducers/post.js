6008b7fca6f6a5e473167f65278c9238b17db2c7
ekalii <andoune0719@gmail.com>
2020-07-04 15:06:33 +0900

    apply redux-saga mainpost and comment

--- reducers/post.js
+++ reducers/post.js
@@ -1,36 +1,113 @@
 export const initialState = {
-	imagePaths: [],
 	mainPosts: [{
+		id: 1,
 		User: {
 			id: 1,
 			nickname: 'Ekalii',
 		},
 		content: 'First Card',
-		img: 'https://avatars0.githubusercontent.com/u/16658067?s=460&u=09f9f385ffec1453896c9b079cc4fd503ee4cded&v=4'
+		img: 'https://avatars0.githubusercontent.com/u/16658067?s=460&u=09f9f385ffec1453896c9b079cc4fd503ee4cded&v=4',
+		comments: [],
 	}],
+	imagePaths: [],
+	addPostErrorReason: '',
+	isAddingPost: false,
+	postAdded: false,
+	isAddingComment: false,
+	addCommentErrorReason: '',
+	commentAdded: false,
 };
 
-const ADD_POST = 'ADD_POST';
-const ADD_DUMMY = 'ADD_DUMMY';
+const dummyPost = {
+	id: 2,
+	User: {
+		id: 1,
+		nickname: 'mockuser',
+	},
+	content: 'Dummy Post',
+	comments: [],
+}
 
-export const addPost = {
-	type: ADD_POST,
-};
+const dummyComment = {
+	id: 1,
+	User: {
+		id: 1,
+		nickname: 'MockUserComment'
+	},
+	createdAt: new Date(),
+	content: 'Dummy Comment',
+}
 
-export const addDummy = {
-	type: ADD_DUMMY,
-};
+export const ADD_POST_REQUEST = 'ADD_POST_REQUEST';
+export const ADD_POST_SUCCESS = 'ADD_POST_SUCCESS';
+export const ADD_POST_FAILURE = 'ADD_POST_FAILURE';
+
+export const ADD_COMMENT_REQUEST = 'ADD_COMMENT_REQUEST';
+export const ADD_COMMENT_SUCCESS = 'ADD_COMMENT_SUCCESS';
+export const ADD_COMMENT_FAILURE = 'ADD_COMMENT_FAILURE';
+
+export const addPostRequest = data => ({
+	type: ADD_POST_REQUEST,
+	payload: {
+		addPostData: data,
+	}
+});
+
+export const addCommentRequest = data => ({
+	type: ADD_COMMENT_REQUEST,
+	payload: {
+		addCommentData: data,
+	},
+});
 
 export default (state = initialState, action) => {
-	const { type } = action;
+	const { type, payload, error } = action;
 	switch (type) {
-		case ADD_POST:
+		case ADD_POST_REQUEST:
 			return {
 				...state,
+				isAddingPost: true,
+				postAdded: false,
+				addPostErrorReason: '',
 			};
-		case ADD_DUMMY:
+		case ADD_POST_SUCCESS:
 			return {
 				...state,
+				isAddingPost: false,
+				postAdded: true,
+				mainPosts: [dummyPost, ...state.mainPosts],
+			};
+		case ADD_POST_FAILURE:
+			return {
+				...state,
+				isAddingPost: false,
+				addPostErrorReason: error,
+			};
+		case ADD_COMMENT_REQUEST:
+			return {
+				...state,
+				isAddingComment: true,
+				commentAdded: false,
+				addCommentErrorReason: '',
+			};
+		case ADD_COMMENT_SUCCESS:
+			const postIndex = state.mainPosts.findIndex(v => v.id === payload.postId);
+			const post = state.mainPosts[postIndex];
+			const comments = [...post.comments, dummyComment];
+			const mainPosts = [...state.mainPosts];
+			mainPosts[postIndex] = { ...post, comments };
+
+			return {
+				...state,
+				isAddingComment: false,
+				commentAdded: true,
+				mainPosts
+			};
+		case ADD_COMMENT_FAILURE:
+			return {
+				...state,
+				isAddingComment: false,
+				addCommentErrorReason: error,
 			};
 		default:
 			return state;
---
+++ saga/post.js
@@ -0,0 +1,63 @@
+import { all, call, fork, put, delay, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+import * as actions from '../reducers/post';
+
+function addPostApi() {
+  return axios.post('/postcard');
+};
+
+function* addPost({ payload }) {
+  try{
+    const { addPostData } = payload;
+    console.log('Add post data: ', addPostData);
+    yield delay(2000);
+    yield put({
+      type: actions.ADD_POST_SUCCESS,
+    });
+  } catch(error) {
+    console.error('Add post error: ', error);
+    yield put({
+      type: actions.ADD_POST_FAILURE,
+      error,
+    });
+  }
+}
+
+function addCommentApi() {
+  return axios.post('/addcomment');
+}
+
+function* addComment({ payload }) {
+  try  {
+    const { addCommentData } = payload;
+    console.log('Add comment data: ', addCommentData);
+    yield delay(2000);
+    yield put({
+      type: actions.ADD_COMMENT_SUCCESS,
+      payload: {
+        postId: addCommentData.postId,
+      },
+    });
+  } catch(error) {
+    console.error('Add comment error: ', error);
+    yield put({
+      type: actions.ADD_COMMENT_FAILURE,
+      error
+    });
+  }
+}
+
+function* watchAddPost() {
+  yield takeLatest(actions.ADD_POST_REQUEST, addPost);
+}
+
+function* watchAddComment() {
+  yield takeLatest(actions.ADD_COMMENT_REQUEST, addComment);
+}
+
+export default function* postSaga() {
+  yield all([
+    fork(watchAddPost),
+    fork(watchAddComment)
+  ])
+}
