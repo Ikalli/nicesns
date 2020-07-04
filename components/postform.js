703b9828fac2ee4416070a83541722ca37e0522d
ekalii <andoune0719@gmail.com>
2020-07-04 15:13:23 +0900

    add onSubmit and onChange

--- components/postform.js
+++ components/postform.js
@@ -1,20 +1,43 @@
 import propTypes from 'prop-types';
 import { Form, Input, Button } from 'antd';
+import { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as actions from '../reducers/post';
 
 export default function PostForm({ imagePaths }) {
+	const dispatch = useDispatch();
+	const { imagePaths, isAddingPost, postAdded } = useSelector(state => state.post);
+	const [ text, setText ] = useState('');
+
+	useEffect(() => {
+		if (postAdded) {
+			setText('');
+		}
+	}, [postAdded]);
+
+	const onSubmitForm = e => {
+		e.preventDefault();
+		dispatch(actions.addPostRequest({
+			text,
+		}));
+	};
+
+	const onChangeText = e => {
+		setText(e.target.value);
+	};
+
 	return(
 		<>
-			<Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data">
-				<Input.TextArea maxLength={140} placeholder="How was your day?"/>
+			<Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onSubmit={onSubmitForm}>
+				<Input.TextArea maxLength={140} placeholder="How was your day?" value={text} onChange={onChangeText}/>
 				<div>
 					<input type="file" multiple hidden/>
 					<Button>Upload Image</Button>
-					<Button type="primary" htmlType="submit">Write</Button>
+					<Button type="primary" htmlType="submit" loading={isAddingPost}>Write</Button>
 				</div>
 				<div>
 					{
-						!!imagePaths
-						&& imagePaths.map(v => {
+						imagePaths.map(v => {
 							return(
 								<div key={v}>
 									<img src={`http://localhost:3000/${v}`} alt={v}/>
