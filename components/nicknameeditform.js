d28c9998d36b6ab053e2fa4f39d3fb0ba80fa264
ekalii <andoune0719@gmail.com>
2020-07-05 20:15:43 +0900

    border and margin fix

--- components/nicknameeditform.js
+++ components/nicknameeditform.js
@@ -2,9 +2,9 @@ import { Form, Input, Button } from 'antd';
 
 export default function NicknameEditForm() {
 	return (
-		<Form style={{ marginTop: '20px' }}>
+		<Form style={{ marginTop: '20px', borderBottom: '1px solid #d9d9d9' }}>
 			<Input style={{ marginBottom: '10px' }} addonBefore="Nickname" />
-			<Button type="primary">Change</Button>
+			<Button type="primary" style={{ marginBottom: '5px' }}>Change</Button>
 		</Form>
 	);
 }
